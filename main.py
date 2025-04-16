6dbb382ae5b3ce568a2cd04da9181c585479dc5d
SaraAkbar16 <114601102+SaraAkbar16@users.noreply.github.com>
2025-04-16 14:56:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-#added this comment
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello World</h1>"
+    return "<h1>Hello DevOps HT4</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
